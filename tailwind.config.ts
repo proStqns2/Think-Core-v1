62ee5b0f193b691527b90ec62e4fa008b97b9206
RYAD YAZIDI <ryadyazidi040@gmail.com>
2025-07-03 02:43:53 +0000

    plus

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -88,10 +88,15 @@ export default {
             height: '0',
           },
         },
+        'grid-pan': {
+          '0%': { backgroundPosition: '0% 0%' },
+          '100%': { backgroundPosition: '50px 50px' },
+        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'grid-pan': 'grid-pan 10s linear infinite',
       },
     },
   },
